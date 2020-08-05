e5ee01c5afc67ee07ae7d4b4c23c9cec761fcd3d
kozai <kozaitoru@basicinc.jp>
2020-08-05 14:13:31 +0900

    token fix

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const BOT_TOKEN = 'NzM4MDE5NjE3NzAzNzIzMDA4.XyF0VA.jBmb0unOLr14OzLuqdgJUOGJFzc';
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
