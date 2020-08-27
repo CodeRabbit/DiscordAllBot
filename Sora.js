baea2b01f4e40eb7b37afe8f488da022d5f5965f
kozai <kozaitoru@basicinc.jp>
2020-08-27 23:22:03 +0900

    feature(Sora): そらちゃんのペットな機能追加
    そらちゃんのペットな機能追加
    
     #45

--- Sora.js
+++ Sora.js
@@ -88,5 +88,59 @@ class Sora{
       +"\n"
     );
   }
+
+  feelingReply(){
+    const feeling_keyword = ['そらちゃんの気持ち', 'そらちゃんのきもち', '猫の気持ち',
+                             '猫のきもち', 'ねこのきもち', 'ねこの気持ち'
+                            ];
+    const feeling_message = ['みんな大好き〜！', 'なでて〜', 'あそんで〜！'];
+    return this.sendRandomMessage(feeling_keyword, feeling_message);
+  }
+
+  callMe(){
+    const call_keyword = ['そらちゃん', 'そーらちゃん', 'そらちゃーん',
+                          'そ〜らちゃん', 'そらちゃ〜ん'
+                         ];
+    const call_message = ['にゃーん', 'にゃ〜ん', 'ﾐｬｰ!', 'にゃ〜', 'にゃー！'];
+    return this.sendArrayMessage(call_keyword, call_message);
+  }
+
+  greeting(){
+    const greeting_keyword = ['おやすみ','おやすみなさい', 'おやすみなさーい',
+                              'おはよー', 'おはよう', 'おっはー', 'こんにちは',
+                              'こんばんは','こんばんわ', 'ただいま', 'ちゃっす',
+                              'またね', 'ばいばい', 'さようなら', 'さらば'
+                            ];
+    const greeting_message = ['にゃん', 'にゃーん', 'にゃ〜？',
+                              'にゃー！', 'み〜！', 'にゃ〜〜'
+                            ];
+    this.sendArrayMessage(greeting_keyword, greeting_message);
+  }
+
+  // キーワード配列の中に完全一致するものがあるときにメッセージ配列の中からランダムで送信
+  sendRandomMessage(keyword, send_message){
+    let send_flag = false;
+    if (keyword.indexOf(this.message.content) !== -1) {
+      let rnd_num = Math.floor(Math.random() * send_message.length);
+      this.message.channel.send(send_message[rnd_num]);
+      send_flag = true;
+    }
+    return send_flag;
+  }
+  // 部分一致でUserメッセージ内にキーワードが含まれていたらメッセージ配列の中からランダムで送信
+  sendArrayMessage(keyword, send_message){
+    let keyword_flag = false;
+    for(let i = 0; i < keyword.length; i++){
+      if(this.message.content.indexOf(keyword[i]) !== -1){
+        keyword_flag = true;
+        break;
+      }
+    }
+    if(keyword_flag){
+      let message_index = Math.floor( Math.random() * send_message.length );
+      this.message.channel.send(send_message[message_index]);
+    }
+    return keyword_flag;
+  }
 }
 module.exports = Sora
