8e7f79d4af5251cc6653c2889446baee7c228ee1
kozai <kozaitoru@basicinc.jp>
2020-11-06 03:31:28 +0900

    ダイス機能の追加

--- src/Sora.js
+++ src/Sora.js
@@ -125,7 +125,7 @@ class Sora extends Pet{
       let index = this.message.content.indexOf("d",1);
       let num = this.message.content.slice(0,index);
       let max = this.message.content.slice(index+1);
-      this.message.channel.send(Math.floor(Math.random() * Math.floor(num*max))+1);
+      this.message.channel.send(Math.floor(Math.random() * Math.floor(num*max))+num);
     } catch(e) {
       console.log(e);
     }
